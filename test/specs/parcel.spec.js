02229bcc3e7ead0f4609e53b931810a6ba46acb5
Mayank Solanki <manky18@gmail.com>
2025-04-28 14:08:02 +0100

    adding basic validation for the land parcel api response

--- test/specs/parcel.spec.js
+++ test/specs/parcel.spec.js
@@ -1,13 +1,15 @@
 import request from 'supertest'
 
-describe('GET /health', () => {
+describe('GET /parcel', () => {
   it('should return success', async () => {
     const res = await request(global.baseUrl)
-      .get('/health')
+      .get('/parcel/SX0679-9238')
       .set('Accept', 'application/json')
       .expect(200)
       .expect('Content-Type', 'application/json; charset=utf-8')
 
-    expect(res.body).toEqual({ message: 'success' })
+    expect(res.body.message).toEqual('success')
+    expect(res.body.parcel.parcelId).toEqual('9238')
+    expect(res.body.parcel.sheetId).toEqual('SX0679')
   })
 })
