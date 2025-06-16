67f2f891a897782598b689dda1eb946682a1ce09
Narahari Miriyala <miriyalanarahari@gmail.com>
2025-06-16 15:07:59 +0100

    added tests for multiple parcels endpoint

---
+++ test/specs/parcelsActions.spec.js
@@ -0,0 +1,64 @@
+import request from 'supertest'
+import { runTestsAndRecordResults } from '../utils/recordResults.js'
+import { PARCELS_ENDPOINT } from '../utils/apiEndpoints.js'
+import { validateResponse } from '../utils/testRunnerHelper.js'
+import {
+  validateParcelFields,
+  validateStatusCode,
+  validateSuccessMessage,
+  validateErrorMessage,
+  validateParcelsStructure,
+  validateActionCode
+} from '../utils/parcelsHelper.js'
+import { cleanupAllure } from '../utils/allureHelper.js'
+
+// Add afterAll hook to clean up resources
+afterAll(async () => {
+  await cleanupAllure()
+})
+
+describe('Parcels endpoint', () => {
+  it('should validate parcels from CSV data', async () => {
+    const dataFile = './test/data/parcelsActionsData.csv'
+
+    // validating each test case
+    const validateParcel = async (testCase, options = {}) => {
+      const parcelIds = testCase.parcelIds.split(',')
+      const fields = testCase.fields.split(',')
+
+      // Make the real API request
+      const response = await request(global.baseUrl)
+        .post(PARCELS_ENDPOINT)
+        .send({ parcelIds, fields })
+        .set('Accept', 'application/json')
+
+      // Validate basic status code match before other validations
+      validateStatusCode(response, testCase)
+
+      // For 200 responses, perform detailed validations
+      if (response.status === 200) {
+        // Validate success message
+        validateSuccessMessage(response, testCase)
+        // Validate parcel structure
+        validateParcelsStructure(response, testCase)
+        // Validate action code
+        validateActionCode(response, testCase)
+      } else {
+        // Validate error message for non-200 responses
+        validateErrorMessage(response, testCase)
+      }
+
+      // Validate the full response using our utility
+      validateResponse(response, testCase, {
+        customValidators: [validateParcelFields],
+        allureReport: options.allureReport,
+        throwOnError: true // Ensure errors are thrown to fail the test
+      })
+
+      return response
+    }
+
+    // Run tests with our helper that handles test result tracking
+    await runTestsAndRecordResults(dataFile, validateParcel)
+  })
+})
--- test/specs/parcelsMultipleFields.spec.js
+++ test/specs/parcelsMultipleFields.spec.js
@@ -1,19 +1,19 @@
 import request from 'supertest'
+import { runTestsAndRecordResults } from '../utils/recordResults.js'
+import { PARCELS_ENDPOINT } from '../utils/apiEndpoints.js'
 import { validateResponse } from '../utils/testRunnerHelper.js'
-import { runTestsAndRecordResults } from '../utils/recordResults'
-import { PARCELS_ENDPOINT } from '../utils/apiEndpoints'
 import {
   validateParcelFields,
   validateStatusCode,
   validateSuccessMessage,
   validateErrorMessage,
-  validateParcelStructure,
+  validateParcelsStructure,
   validateSizeUnit,
   validateSizeValue,
   validateActionCode,
   validateAvailableArea
-} from '../utils/parcelHelper'
-import { cleanupAllure } from '../utils/allureHelper'
+} from '../utils/parcelsHelper.js'
+import { cleanupAllure } from '../utils/allureHelper.js'
 
 // Add afterAll hook to clean up resources
 afterAll(async () => {
@@ -22,17 +22,19 @@ afterAll(async () => {
 
 describe('Parcels endpoint', () => {
   it('should validate parcels from CSV data', async () => {
-    const dataFile = './test/data/parcelData.csv'
+    const dataFile = './test/data/parcelsMultipleFieldsData.csv'
 
-    // Validate a single parcel test case
+    // validating each test case
     const validateParcel = async (testCase, options = {}) => {
-      const { sheetId, parcelId } = testCase
-      const parcelIdentifier = `${sheetId}-${parcelId}`
+      const parcelIds = testCase.parcelIds.split(',')
+      const fields = testCase.fields.split(',')
 
       // Make the real API request
       const response = await request(global.baseUrl)
-        .get(`${PARCELS_ENDPOINT}/${parcelIdentifier}`)
+        .post(PARCELS_ENDPOINT)
+        .send({ parcelIds, fields })
         .set('Accept', 'application/json')
+
       // Validate basic status code match before other validations
       validateStatusCode(response, testCase)
 
@@ -41,7 +43,7 @@ describe('Parcels endpoint', () => {
         // Validate success message
         validateSuccessMessage(response, testCase)
         // Validate parcel structure
-        validateParcelStructure(response)
+        validateParcelsStructure(response, testCase)
         // Validate size unit
         validateSizeUnit(response, testCase)
         // Validate size value
@@ -67,19 +69,5 @@ describe('Parcels endpoint', () => {
 
     // Run tests with our helper that handles test result tracking
     await runTestsAndRecordResults(dataFile, validateParcel)
-  })
-})
-
-describe('Parcels endpoint', () => {
-  it('should validate individual parcels when not passed as csv data', async () => {
-    const res = await request(global.baseUrl)
-      .get(`${PARCELS_ENDPOINT}/SD6743-6006`)
-      .set('Accept', 'application/json')
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
-
-    expect(res.body.message).toEqual('success')
-    expect(res.body.parcel.parcelId).toEqual('6006')
-    expect(res.body.parcel.sheetId).toEqual('SD6743')
   })
 })
