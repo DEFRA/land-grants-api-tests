46e98ea646add0339ed0be038a6fbf6210bff9b6
andrewkeig <andrew.keig@gmail.com>
2026-05-27 16:22:57 +0100

    add more logging

--- test/utils/secure-context.js
+++ test/utils/secure-context.js
@@ -3,23 +3,26 @@ import tls from 'node:tls'
 function getTrustStoreCerts(envs) {
   console.log('Extracting trust store certificates from environment variables')
 
-  console.log(envs)
-
   return Object.entries(envs)
     .map(([key, value]) => key.startsWith('TRUSTSTORE_') && value)
-    .filter(
-      /** @returns {envValue is string} */
-      (envValue) => Boolean(envValue)
-    )
+    .filter((envValue) => {
+      console.log('envValue')
+      console.log(envValue)
+      return (envValue) => Boolean(envValue)
+    })
     .map((envValue) => Buffer.from(envValue, 'base64').toString().trim())
 }
 
 export const createSecureContext = () => {
+  console.log('createSecureContext called')
   const originalTlsCreateSecureContext = tls.createSecureContext
 
   tls.createSecureContext = function (options = {}) {
+    console.log('tls.createSecureContext called')
+    console.log(JSON.stringify(options, null, 2))
     const trustStoreCerts = getTrustStoreCerts(process.env)
-
+    console.log('trustStoreCerts')
+    console.log(trustStoreCerts)
     const tlsSecureContext = originalTlsCreateSecureContext(options)
 
     trustStoreCerts.forEach((cert) => {
@@ -29,5 +32,6 @@ export const createSecureContext = () => {
     return tlsSecureContext
   }
 
+  console.log('createSecureContext returning')
   return tls.createSecureContext()
 }
