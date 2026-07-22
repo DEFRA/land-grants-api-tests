748e19adaaa43f5cc55014b8da9a2fd06c188529
Narahari Miriyala <miriyalanarahari@gmail.com>
2026-07-22 16:24:11 +0100

    add parcels test with agreements from DAL

---
+++ test/specs/parcels_v2_with_DAL_agreements.spec.js
@@ -0,0 +1,96 @@
+import request from 'supertest'
+import { runTestsAndRecordResults } from '../utils/recordResults.js'
+import {
+  PARCELS_ENDPOINT_V2,
+  BEARER_TOKEN,
+  API_KEY
+} from '../utils/apiEndpoints.js'
+import {
+  validateStatusCode,
+  validateSuccessMessage,
+  validateParcelsStructure,
+  validateActionCode,
+  validateAvailableArea,
+  validateSizeUnit,
+  validateSizeValue,
+  validateSSSIConsentRequired,
+  validateHEFERRequired,
+  validateErrorMessage,
+  validateActionGroups
+} from '../utils/parcelsHelper.js'
+
+describe('Parcels V2 endpoint', () => {
+  it('Should validate version2 parcels size, actions, SSSI Consent Required and available area taking existing agreements and planned actions into account', async () => {
+    const dataFiles = [
+      './test/data/sfi/parcels/parcelsData_groups.csv',
+      './test/data/sfi/parcels/parcelsData_CMOR1,UPL1,UPL2andUPL3_v2.csv',
+      './test/data/sfi/parcels/parcelsData_UPL8andUPL10_v2.csv',
+      './test/data/sfi/parcels/parcelsData_CLIG3_v2.csv',
+      './test/data/sfi/parcels/parcelsData_CSAM3_v2.csv'
+    ]
+
+    // validating each test case
+    const validateParcel = async (testCase, options = {}) => {
+      const parcelIds = testCase.parcelIds.split(',')
+      const fields = testCase.fields.split(',')
+      const sbi =
+        testCase.sbi && testCase.sbi.trim() !== '' ? testCase.sbi : '0123456789' // Default SBI if not provided
+
+      // Make the real API request
+      let response
+      if (testCase.plannedActions) {
+        const plannedActions = JSON.parse(testCase.plannedActions)
+        response = await request(global.baseUrl)
+          .post(PARCELS_ENDPOINT_V2)
+          .send({ parcelIds, fields, plannedActions, sbi })
+          .set('Accept', 'application/json')
+          .set('Authorization', `Bearer ${BEARER_TOKEN}`)
+          .set('x-api-key', API_KEY || '')
+          .set('Accept-Encoding', '*')
+          .set('X-Forwarded-Authorization', 'TestToken') // Add this header to simulate the presence of the X-Forwarded-Authorization header
+      } else {
+        response = await request(global.baseUrl)
+          .post(PARCELS_ENDPOINT_V2)
+          .send({ parcelIds, fields, sbi })
+          .set('Accept', 'application/json')
+          .set('Authorization', `Bearer ${BEARER_TOKEN}`)
+          .set('x-api-key', API_KEY || '')
+          .set('Accept-Encoding', '*')
+          .set('X-Forwarded-Authorization', 'TestToken') // Add this header to simulate the presence of the X-Forwarded-Authorization header
+      }
+
+      // Validate basic status code match before other validations
+      validateStatusCode(response, testCase)
+
+      // For 200 responses, perform detailed validations
+      if (response.status === 200) {
+        // Validate success message
+        validateSuccessMessage(response, testCase)
+        // Validate parcel structure
+        validateParcelsStructure(response, testCase)
+        // Validate size unit
+        validateSizeUnit(response, testCase)
+        // Validate size value
+        validateSizeValue(response, testCase)
+        // Validate action code
+        validateActionCode(response, testCase)
+        // Validate available area value
+        validateAvailableArea(response, testCase)
+        // Validate SSSI consent required
+        validateSSSIConsentRequired(response, testCase)
+        // Validate HEFER required
+        validateHEFERRequired(response, testCase)
+        // validate action groups
+        validateActionGroups(response, testCase)
+      } else {
+        // Validate error message for non-200 responses
+        validateErrorMessage(response, testCase)
+      }
+    }
+
+    // Run tests with our helper that handles test result tracking for each data file
+    for (const dataFile of dataFiles) {
+      await runTestsAndRecordResults(dataFile, validateParcel)
+    }
+  })
+})
