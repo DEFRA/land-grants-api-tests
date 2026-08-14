ce6568ae1f54953bdb3c41c91172907ff1376705
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2026-08-14 14:44:14 +0100

    GSPS-469-CNUM2-ParcelsEndPointCheck

--- test/specs/parcels_v2.spec.js
+++ test/specs/parcels_v2.spec.js
@@ -37,7 +37,7 @@ describe('Parcels V2 endpoint', () => {
       './test/data/sfi/parcels/parcelsData_CNUM2_v2.csv',
       ...(isDevOrLocalEnvironment
         ? [
-            // './test/data/sfi/parcels/AvailableAreaCalculation_Including_DAL_data.csv'
+            './test/data/sfi/parcels/AvailableAreaCalculation_Including_DAL_data.csv'
           ]
         : [])
     ]
